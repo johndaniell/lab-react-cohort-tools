3ea46baf52c01aea8145429bc96d7d78c2fa82ad
johndaniell@yahoo.com <johndaniell@yahoo.com>
2024-04-03 15:37:32 +0200

    forgot a button :D

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,13 +23,15 @@ function Navbar() {
 
         <div className="w-1/4 flex justify-end mr-4">
           {/* User Profile Button */}
-          <button className="flex items-center text-l py-1">
+          <Link to="/profile">
+          <button type="button" className="flex items-center text-l py-1">
             <img
               src="https://education-team-2020.s3.eu-west-1.amazonaws.com/web-dev/labs/lab-cohort-tools-routing/profile-1.png"
               alt="Logo"
               className="h-10 w-auto border-solid border border-white rounded-3xl p-1"
             />
           </button>
+          </Link>
 
         </div>
       </div>
